b927317deac94d94cfaac7c53c9a37013808acf6
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-08 21:00:35 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,12 @@
 module Memorable
  
   module ClassMethods
-  def self.reset_all
-    self.all.clear
+  def reset_all
+    all.clear
   end
 
   def count
-    @@all.count
+    all.count
   end
 end 
   
